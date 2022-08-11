cc22c04c02b8b76cf93c40b89c778b8bc3295913
krishnamurthypranesh <pranesh@creatorstack.com>
2022-08-11 20:29:46 +0530

    :tada: Init frontend with stub

---
+++ frontend/main.elm
@@ -0,0 +1,56 @@
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+
+-- MAIN
+
+main = Browser.sandbox { init = init, update = update, view = view }
+
+-- MODEL
+type alias Model = 
+    {
+        name: String,
+        , password: String,
+        , passwordAgain: String,
+    }
+
+init : Model
+init =
+    Model "" "" ""
+
+-- UPDATE
+type Msg
+    = Name String
+    | Password String
+    | PasswordAgain String
+
+update: Msg -> Model -> Model
+update msg model = 
+    case msg of
+        Name name ->
+            { model | name = name}
+
+    Password password ->
+        { model | password = password }
+
+    PasswordAgain password ->
+        {  model | passwordAgain = password }
+
+-- VIEW
+view: Model -> Html Msg
+view model =
+    div []
+    [
+        viewInput "text" "Name" model.name Name
+        , viewInput "password" "Password" model.password Password
+        , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
+        , viewValidation model 
+    ]
+
+viewInput : Model -> Html msg
+viewValidation model =
+    if model.password == model.passwordAgain then
+        div [ style "color" "green" ] [ text "OK" ]
+    else
+        div [ style "color" "red" ] [ text "Passwords do not match" ]
