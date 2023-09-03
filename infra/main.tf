9f13706eafe4c73c634e8f17311c14b0d3cf8aa6
krishnamurthypranesh <pk96ishere@gmail.com>
2023-09-03 15:05:52 -0400

    Init infra folder

---
+++ infra/main.tf
@@ -0,0 +1,9 @@
+# create vpc
+
+# create lambda and associated resources: ecr
+
+# create api gateway with execution id set to true
+
+# create the codepipeline: source (github), codebuild, codedeploy
+
+# create dynamodb
---
+++ infra/variables.tf
@@ -0,0 +1,17 @@
+# aws provider variables
+variable "aws_region" {
+    type = string
+}
+
+# newrelic provider variables
+variable "new_relic_account_id" {
+    type = string
+}
+
+variable "new_relic_api_key" {
+    type = string
+}
+
+variable "new_relic_region" {
+    type = string
+}
