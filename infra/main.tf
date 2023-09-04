dfab9b5fe2dd80bcd0fec28cad2688782ffbe819
krishnamurthypranesh <pk96ishere@gmail.com>
2023-09-04 14:52:20 -0400

    Fix errors in plan

--- infra/main.tf
+++ infra/main.tf
@@ -40,16 +40,13 @@ data "archive_file" "lambda" {
 }
 
 resource "aws_lambda_function" "painted_porch_backend" {
-  # If the file is not in the current working directory you will need to include a
-  # path.module in the filename.
-  s3_bucket = aws_s3.lambda_deployment_bucket.name
   function_name = "painted_porch_backend"
   role          = aws_iam_role.iam_for_lambda.arn
-  handler       = "app.main.handler"
 
-  source_code_hash = data.archive_file.lambda.output_base64sha256
+  package_type = "Image"
+  image_uri = "${var.ecr_repo_url}:latest"
 
-  runtime = "python3.10"
+  source_code_hash = data.aws_ecr_image.lambda_image_latest.id
 
   environment {
     variables = {
@@ -71,7 +68,7 @@ resource "aws_apigatewayv2_stage" "painted_porch" {
   auto_deploy = true
 
   access_log_settings {
-    destination_arn = aws_cloudwatch_log_group.api_gw.arn
+    destination_arn = aws_cloudwatch_log_group.painted_porch_api_gw.arn
 
     format = jsonencode({
       requestId               = "$context.requestId"
@@ -127,6 +124,16 @@ resource "aws_dynamodb_table" "painted_porch_entries" {
   write_capacity = 5
   hash_key       = "journal_id"
   range_key      = "created_at"
+
+  attribute {
+    name = "journal_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "created_at"
+    type = "N"
+  }
 
   attribute {
     name = "user_id"
